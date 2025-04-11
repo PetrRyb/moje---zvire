cb93ec6671efde12c3b8a68ee151b7fc2ef59ab8
PetrRyb <petr.rybin@infis.cz>
2025-04-11 21:57:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 oblíbenéZvíře= str(input("Jaké je tvoje oblíbené zvíře?"))
 print(f"Tvoje oblíbené zvíře je {oblíbenéZvíře}.")
+TvojeZvire = input(str("Jaké je tvoje zvíře"))
+print ("Tvoje zvíře je",TvojeZvire)
