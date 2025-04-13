5e3139dfb5fda316bf99328e4b07718af7a3baed
ZdenekHauer <zdenek.hauer@infis.cz>
2025-04-13 16:21:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 TvojeZvire = input("Jaké je tvoje zvíře?")  
 print("Tvoje zvíře je", TvojeZvire)
-
-oblibeneZvire = input("Jaké je tvoje oblíbené zvíře?")
-print(f"Tvoje oblíbené zvíře je {oblibeneZvire}.")
